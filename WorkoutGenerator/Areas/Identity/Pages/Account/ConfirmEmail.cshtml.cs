3dae8d24e78aeff1eb6b0b5a3f5d62ca1946010f
Aviv Cohen <kaazz931@gmail.com>
2019-09-03 12:07:05 +0300

    confirmed email

--- WorkoutGenerator/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
+++ WorkoutGenerator/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
@@ -6,15 +6,16 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WorkoutGenerator.Data;
 
 namespace WorkoutGenerator.Areas.Identity.Pages.Account
 {
     [AllowAnonymous]
     public class ConfirmEmailModel : PageModel
     {
-        private readonly UserManager<IdentityUser> _userManager;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public ConfirmEmailModel(UserManager<IdentityUser> userManager)
+        public ConfirmEmailModel(UserManager<ApplicationUser> userManager)
         {
             _userManager = userManager;
         }
