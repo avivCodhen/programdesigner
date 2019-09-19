380acc4ae7baff73d6edb2d9fa559db2bdfcf75c
Aviv Cohen <kaazz931@gmail.com>
2019-09-19 21:13:34 +0300

    tabs change to all

---
+++ WorkoutGenerator/Controllers/ProgressController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WorkoutGenerator.Controllers
+{
+    [Authorize]
+    public class ProgressController : Controller
+    {
+        public IActionResult WorkoutProgress(int workoutId)
+        {
+            return View();
+        }
+    }
+}
--- WorkoutGenerator/Data/MuscleExercises.cs
+++ WorkoutGenerator/Data/MuscleExercises.cs
@@ -7,7 +7,6 @@ namespace WorkoutGenerator.Data
     public class MuscleExercises
     {
         [Key] public int Id { get; set; }
-
         public int WorkoutId { get; set; }
         [ForeignKey("WorkoutId")] public virtual Workout Workout { get; set; }
         public virtual IList<WorkoutExercise> Exercises { get; set; }
