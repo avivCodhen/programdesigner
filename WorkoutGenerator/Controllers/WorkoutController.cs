d2d07d99c1d17ca47df508b01ac41da22f46477e
Aviv Cohen <kaazz931@gmail.com>
2019-09-25 08:34:14 +0300

    WorkoutController fix

--- WorkoutGenerator/Controllers/WorkoutController.cs
+++ WorkoutGenerator/Controllers/WorkoutController.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
-using Castle.DynamicProxy.Generators.Emitters.SimpleAST;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using WorkoutGenerator.Data;
